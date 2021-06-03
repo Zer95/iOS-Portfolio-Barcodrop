63af7b4bd05f96dbe20467471b5eb1981dba0ef2
Precial <ggrty10@gmail.com>
2021-06-03 22:40:50 +0900

    Add Animation From DetialView

--- Barcodrop/Home/DetailViewController.swift
+++ Barcodrop/Home/DetailViewController.swift
@@ -12,6 +12,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var Thumbnail: UIImageView!
     @IBOutlet weak var Name_lable: UILabel!
     @IBOutlet weak var d_day_lable: UILabel!
+    @IBOutlet weak var Name_lableCenterX: NSLayoutConstraint!
+    @IBOutlet weak var d_day_lableCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
     
@@ -20,8 +22,58 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         Thumbnail.layer.cornerRadius = 10
         updateUI()
+        prepareAnimation()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+
+        Name_lable.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        d_day_lable.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        Name_lable.alpha = 0
+        d_day_lable.alpha = 0
+    }
+    
+    private func showAnimation() {
+        
+        UIView.animate(
+                   withDuration: 1,
+                   delay: 0,
+                   usingSpringWithDamping: 0.6,
+                   initialSpringVelocity: 2,
+                   options: .allowUserInteraction,
+                   animations: {
+                       self.Name_lable.transform = CGAffineTransform.identity
+                       self.Name_lable.alpha = 1
+               },
+                   completion: nil)
+               
+        
+        UIView.animate(
+            withDuration: 1,
+            delay: 0.2,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 2,
+            options: .allowUserInteraction,
+            animations: {
+                self.d_day_lable.transform = CGAffineTransform.identity
+                self.d_day_lable.alpha = 1
+        },
+            completion: nil)
+
+        
+        UIView.transition(with: Thumbnail,
+                          duration: 0.3,
+                          options: .transitionFlipFromLeft,
+                          animations: nil, completion: nil)
+
+        
+    }
 
     func updateUI(){
         if let prouctInfo = self.viewModel.productInfo{
