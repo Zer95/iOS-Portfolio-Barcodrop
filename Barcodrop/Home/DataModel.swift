94a4558e8d14266560b6f8b7080a3ef80359eb1d
Precial <ggrty10@gmail.com>
2021-06-08 22:30:53 +0900

    DataModel basic

---
+++ Barcodrop/Home/DataModel.swift
@@ -0,0 +1,23 @@
+//
+//  DataModel.swift
+//  Barcodrop
+//
+//  Created by SG on 2021/06/08.
+//
+
+
+
+/*
+ 
+ PrimaryKey - ? or ROW_NUM
+ 
+ 상품명 (ProductTitle) - String
+ 카테고리 (Category) - String
+ 구매일자 (BuyDay) - Date
+ 유통기한 (EndDay) - Date
+ 알람설정날짜 (AlarmDay) - Array (Int or Date)
+ 알람설정시간 (AlarmTime) - Date or Int or Time
+ 이미지 (ImgFile) - ? or ImgURL
+ 
+
+ */
