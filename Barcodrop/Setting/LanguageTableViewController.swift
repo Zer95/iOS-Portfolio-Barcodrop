9ad4d63f8760b57b2da8eef9cc9d8db1d66ef0d7
Precial <ggrty10@gmail.com>
2021-06-24 15:51:27 +0900

    select Mode Setting From LanguageView

--- Barcodrop/Setting/LanguageTableViewController.swift
+++ Barcodrop/Setting/LanguageTableViewController.swift
@@ -13,16 +13,19 @@ class LanguageTableViewController: UITableViewController {
     @IBOutlet var languagetableView: UITableView!
     @IBOutlet weak var screenView: UIView!
     
+    
+    @IBOutlet weak var selectKr: UILabel!
+    @IBOutlet weak var selectEng: UILabel!
+    
+    var selectLanguage = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBar.barTintColor = .white
         self.navigationController?.navigationBar.tintColor = .black
        // self.navigationController?.navigationBar.topItem?.title = ""
 
 
-
-  
-        
         animationView.animation = Animation.named("language")
         animationView.frame = view.bounds
         animationView.contentMode = .scaleAspectFit
@@ -45,59 +48,21 @@ class LanguageTableViewController: UITableViewController {
         return 2
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("클릭인식 \(indexPath.row)")
+        if indexPath.row == 0 {
+        selectKr.isHidden = false
+        selectEng.isHidden = true
+        self.selectLanguage = "kr"
+        }
+        else if  indexPath.row == 1 {
+        selectKr.isHidden = true
+        selectEng.isHidden = false
+        self.selectLanguage = "eng"
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
+
